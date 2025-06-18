bcbedb0804c6b36b6b23ff733356d2d9e94efc8a
Eris <mccaton00@gmail.com>
2025-06-18 06:14:17 -0500

    Forgot a pkill, comments added

--- personal/vencord.sh
+++ personal/vencord.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-wget  -O /tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb";
-sudo dpkg -i /tmp/discord.deb;
-rm /tmp/discord.deb;
-screen -dm discord;
-pkill -f "discord";
-sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+wget  -O /tmp/discord.deb "https://discord.com/api/download?platform=linux&format=deb"; #grab latest discord deb and throw in temp
+pkill -f "discord"; #kill any active discord clients
+sudo dpkg -i /tmp/discord.deb; #install updated discord
+rm /tmp/discord.deb; #clean up work
+screen -dm discord; #silently run discord, needed before installing vencord
+pkill -f "discord"; #lazy way of killing that hidden discord
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)" #vencord installer
